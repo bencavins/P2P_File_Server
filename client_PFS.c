d914e29589677877a945aa64b7a31da4aa63f83c
cavinsb <benjamin.cavins@colorado.edu>
2013-11-23 16:17:46 -0700

    Added argument extraction

--- client_PFS.c
+++ client_PFS.c
@@ -14,11 +14,22 @@
 
 int main(int argc, char *argv[]) {
 
+	char *client_name;
+	char *server_ip;
+	int server_port;
+
 	if (argc < ARG_MIN) {
 		fprintf(stderr, "Usage: %s %s\n", argv[0], USAGE);
 		return EXIT_FAILURE;
 	}
 
-	printf("Hello client\n");
+	client_name = argv[1];
+	server_ip = argv[2];
+	server_port = atoi(argv[3]);
+
+	printf("client name = %s\n", client_name);
+	printf("server ip = %s\n", server_ip);
+	printf("server port = %d\n", server_port);
+
 	return 0;
 }
