6e0ace7d9da6e8d5b5d7da8293f733ad35f85b41
cavinsb <benjamin.cavins@colorado.edu>
2013-11-23 16:44:04 -0700

    Added arg checking and extraction

--- server_PFS.c
+++ server_PFS.c
@@ -7,8 +7,23 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-	printf("Hello server\n");
-	return 0;
+#define USAGE "<port number>"
+#define ARG_MIN 1
+
+int main(int argc, char *argv[]) {
+
+	int port;
+
+	if (argc < ARG_MIN + 1) {
+		fprintf(stderr, "Usage: %s %s\n", argv[0], USAGE);
+		return EXIT_FAILURE;
+	}
+
+	port = atoi(argv[1]);
+
+	printf("port = %d\n", port);
+
+	return EXIT_SUCCESS;
 }
